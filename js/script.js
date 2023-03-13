d5f7cb906dd5ac54e51cc865d649508ffa235410
Isaac Ayeni <ayeniisaac1on1@gmail.com>
2023-03-13 19:48:44 +0100

    First Commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements =document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('app-show')
+        }else{
+            entry.target.classList.remove('app-show')
+        }
+    })
+})
+
+
+const hiddenElements1 =document.querySelectorAll('.app-hidden');
+hiddenElements.forEach((el) => observer.observe(el));
